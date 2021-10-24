64f2830c9f78a95fa773350372d5b7c0dad0389a
Ben Soille <ben@soille.fr>
2021-10-24 19:11:05 +0200

    Fucking relations.... No doc...

--- src/databaseConnection.ts
+++ src/databaseConnection.ts
@@ -6,7 +6,8 @@ mongoose.Promise = global.Promise;
 const connectToDatabase = async (): Promise<void> => {
   const options: ConnectionOptions = { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true };
 
-  await mongoose.connect(`mongodb://${config.mongo_user_name}:${config.mongo_user_pass}@${config.mongo_host}:${config.mongo_port}/${config.mongo_db_name}`, options);
+  // await mongoose.connect(`mongodb://${config.mongo_user_name}:${config.mongo_user_pass}@${config.mongo_host}:${config.mongo_port}/${config.mongo_db_name}`, options);
+  await mongoose.connect(`mongodb://${config.mongo_host}:${config.mongo_port}/${config.mongo_db_name}`, options);
 };
 
 export { connectToDatabase };
